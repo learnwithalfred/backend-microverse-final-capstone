95e0aa34c945ac6afca04a88827c7450fa607538
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-30 13:45:49 +0100

    Finish authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :cars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
 end
