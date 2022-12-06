352a08b74d30deaa221c0c0ae405285297691c6a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-06 11:40:49 +0100

    Remove routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :cars
-  resources :users, except: [:create]
   devise_for :users,
              controllers: {
                  sessions: 'users/sessions',
