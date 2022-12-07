524899cfb449aa6061cbf71cb13a914645434c00
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-07 07:33:17 +0000

    Add model validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,13 @@
 class Car < ApplicationRecord
+  MAX_LENGTH = 250
+  MIN_NUM = 1
+  MAX_NUM = 10_000_000
+
+  validates :name, presence: true, length: { minimum: MIN_NUM, maximum: MAX_LENGTH }
+  validates :brand, presence: true, length: { minimum: MIN_NUM, maximum: MAX_LENGTH }
+  validates :image, presence: true, length: { minimum: MIN_NUM}
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: MIN_NUM, less_than_or_equal_to: MAX_NUM }
+  validates :total_amount_payable, presence: true, numericality: { greater_than_or_equal_to: MIN_NUM, less_than_or_equal_to: MAX_NUM }
+  validates :option_to_purchase_fee, presence: true, numericality: { greater_than_or_equal_to: MIN_NUM, less_than_or_equal_to: MAX_NUM }
+
 end
