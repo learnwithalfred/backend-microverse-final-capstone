4ab3c5d4f95dc34c980b053d86a9660f9fd551ef
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-07 10:55:21 +0000

    Setup rspec controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
