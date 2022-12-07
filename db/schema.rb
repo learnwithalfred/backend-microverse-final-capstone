2e629960fdb772c02d7178b43b0d1eedb9bbeb50
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-07 07:33:53 +0000

    Add description to cars model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_151244) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_070700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_151244) do
     t.integer "option_to_purchase_fee", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", default: "A new car"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
