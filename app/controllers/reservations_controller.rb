c403b0f727f3e427732ab8892653442c426d7433
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-08 17:36:30 +0000

    Update controllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @reservations = Reservation.where(user_id: current_user.id)
