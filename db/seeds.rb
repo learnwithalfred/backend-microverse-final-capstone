bbc34f969d6dc9559a9b679d48856dfb864447f9
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-12-10 13:54:24 +0100

    Update schema file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,19 @@
 
 Car.create([
   {
-    name:"Plaid", image:"https://drive.google.com/uc?id=1GqSZ6nKXqn-6SgzGEp3bQajI4ojr7aB8", brand:"Tesla", duration:200, total_amount_payable:500, option_to_purchase_fee:900
+    name:"MODEL 3", image:"https://i.ibb.co/y6YVSjz/tesla2.png", brand:"Tesla", duration:200, total_amount_payable:500, option_to_purchase_fee:900
   },  {
-    name:"Camry", image:"https://drive.google.com/uc?id=1Qlv93aNOyPyrqd-nhcPXaY91lvylAp3l", brand:"Toyota", duration:89, total_amount_payable:67, option_to_purchase_fee:900
+    name:"MODEL X", image:"https://i.ibb.co/bv2ZfQX/tesla1.png", brand:"Tesla", duration:89, total_amount_payable:67, option_to_purchase_fee:900
   },
     {
-    name:"Almera", image:"https://drive.google.com/uc?id=18fJvKkGZ5hLK5mKSEHZj9-9eUspby9h4", brand:"Benz", duration:89, total_amount_payable:89, option_to_purchase_fee:899
+    name:"MODEL S", image:"https://i.ibb.co/26M9fRg/tesla4.png", brand:"Tesla", duration:89, total_amount_payable:89, option_to_purchase_fee:899
   },
     {
-    name:"Tesla 67", image:"https://drive.google.com/uc?id=1pcNDQfYSD_CZHHyDI580fYw4xhcv6Igl", brand:"Benz 9000", duration:67, total_amount_payable:8989, option_to_purchase_fee:890
+    name:"MODEL Y", image:"https://i.ibb.co/SrSkx5X/tesla3.png", brand:"Tesla", duration:67, total_amount_payable:8989, option_to_purchase_fee:890
   },
     {
-    name:"Honda 202", image:"https://drive.google.com/uc?id=12XdiD7EtVqpV-elJSIwWKX_v7kwshRwG", brand:"Benz", duration:67, total_amount_payable:8989, option_to_purchase_fee:890
+    name:"MODEL Y", image:"https://i.ibb.co/SrSkx5X/tesla3.png", brand:"Tesla", duration:67, total_amount_payable:8989, option_to_purchase_fee:890
   }
 ])
+
+User.create(role: "admin", name: "admin", email: "admin@gmail.com", password: "111111")
