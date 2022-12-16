21a4f5a216dd0ab57475a3e9a00cba8b52d84a1b
Eric <tuyishimireericc@gmail.com>
2022-12-16 18:37:41 +0200

    updated the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -21,14 +20,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
+  
   def set_user
     @user = User.find(params[:id])
   end
