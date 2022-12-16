353cb4ea0885002327b606eb2182546c70eeecef
Eric <tuyishimireericc@gmail.com>
2022-12-16 18:45:39 +0200

    Added testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,20 +31,10 @@ Rails.application.configure do
 
   config.active_support.deprecation = :log
 
-  # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
 
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
