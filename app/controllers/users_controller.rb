481f649ad8784eeab07359798c661d7c24eb712d
Eric <tuyishimireericc@gmail.com>
2022-12-16 18:39:36 +0200

    Updated the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,12 +25,11 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name, :role)
   end
