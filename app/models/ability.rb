ea4d8ca8a3a93842d2ffb9d8b1af29c8fdd006d2
Alfred Boateng <programmingwithalfred@gmail.com>
2023-01-09 07:57:19 +0000

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.admin?
-
+   
     return unless user.present?
 
     can :read, Car
     can :manage, User, id: user.id
+    can :manage, :all if user.admin?
   end
 end
